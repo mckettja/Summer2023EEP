5cfd098e4652c41b473ff051a4297522bc7377c6
Josh McKettrick <mckettja@mail.uc.edu>
2023-06-08 17:19:22 -0400

    app boilerplate #2
    added basic React.js boilerplate template

---
+++ summer-eep-2023/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+
+      </div>
+
+      <header>
+
+      </header>
+
+      <body>
+        <h1>Here is where I will paste my JSX code to be displayed</h1>
+      </body>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
